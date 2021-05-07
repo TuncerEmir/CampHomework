e8b8ee2df7a2cfa6538322ff833e082ebdf726e2
emir Tuncer <emirtuncer76@outlook.com>
2021-05-08 02:53:54 +0300

    08.05.2021

--- eCommerce/src/eCommerce/Main.java
+++ eCommerce/src/eCommerce/Main.java
@@ -10,12 +10,12 @@ import eCommerce.entities.concretes.User;
 public class Main {
 
 	public static void main(String[] args) {
-		//UserManager userManager=new UserManager(new InMemoryUserDao(), new AmazonMailManagerAdapter());
+		UserManager userManager=new UserManager(new InMemoryUserDao(), new AmazonMailManagerAdapter());
 		User mert=new User(4, "Mert", "Çakmak", "mertcakmak@gmail.com", "123654", true);
-		//userManager.add(mert);
+		userManager.add(mert);
 		
 		User enes = new User(2, "Hilmi", "Þafak", "hilmisafak@gmail.com", "147258", false);
-		//userManager.update(enes);
+		userManager.update(enes);
 		
 		InMemoryUserDao inMemoryUserDao = new InMemoryUserDao();
 		GoogleMailManagerAdapter googleMailManagerAdapter = new GoogleMailManagerAdapter();
@@ -25,7 +25,7 @@ public class Main {
 		AuthManager authManager =new AuthManager(new UserManager(inMemoryUserDao, googleMailManagerAdapter));
 		//authManager.register(mert);
 		authManager.register(mert);
-		UserManager userManager = new UserManager(inMemoryUserDao, amazonMailManagerAdapter);
+		//UserManager userManager = new UserManager(inMemoryUserDao, amazonMailManagerAdapter);
 		
 		userManager.getAll();
 		//userManager.getAll();		
